ecb69005f184298f5e0cee8ec2eae75e17fa213c
Hidayt Rahman <hidaytrahman@gmail.com>
2022-05-14 13:31:43 +0530

    feat(improvements): Router and animated text

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Logo from 'components/entity/logo/Logo';
 import './App.css';
 import Twitter from './components/twitter/Twitter';
 
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-          <h1>MemeSnap</h1>
-          <h2>Generate memes of social media post</h2>
-      </header>
+    <BrowserRouter>
 
-      <section>
-        <Twitter />
-      </section>
-      
-      
-    </div>
+      <div className="App"> 
+        <Logo />
+        <header className="App-header">
+          <h2>Generate memes of social media post</h2>
+        </header>
+
+
+        <Routes>
+          <Route exact path="/" element={<h1>Home Page</h1>} />
+          <Route exact path="page1" element={<Home />} />
+          <Route exact path="page2" element={<About />} />
+        </Routes>
+
+        <section>
+          <Twitter />
+        </section>
+
+
+      </div>
+
+    </BrowserRouter>
+
+
   );
 }
 
---
+++ src/components/entity/animation/TextAnimation.js
@@ -0,0 +1,7 @@
+import "./TextAnimation.css";
+
+const TextAnimation = () => {
+    return <h1 class="ml2">Sunny mornings</h1>
+}
+
+export default TextAnimation;
